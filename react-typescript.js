4aac6b20c7784e0cdae5c928d17e953e9ea53b82
andreystepanov <code@andrey.co>
2020-09-13 07:40:11 -0400

    Added options to react-typescript config

--- react-typescript.js
+++ react-typescript.js
@@ -1,5 +1,8 @@
 const config = require('./react')
 
-config.presets.push('@babel/preset-typescript')
+config.presets.push([
+  '@babel/preset-typescript',
+  { isTSX: true, allExtensions: true },
+])
 
 module.exports = config
