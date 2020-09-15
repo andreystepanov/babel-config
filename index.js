fc4dd77b8cb324aab7fb522201860d1d6d65a167
andreystepanov <code@andrey.co>
2020-09-15 17:49:45 +0300

    Added new babel plugin

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ module.exports = {
   ],
   plugins: [
     '@babel/plugin-proposal-export-default-from',
+    [
+      'babel-plugin-transform-async-to-promises',
+      { inlineHelpers: true, externalHelpers: true },
+    ],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     '"@babel/proposal-object-rest-spread',
     'babel-plugin-lodash',
